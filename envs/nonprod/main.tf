5be00fcf6f9b001b838c293742ae5ebd31c731c0
KonpalM <138207736+KonpalM@users.noreply.github.com>
2025-10-28 22:54:32 +0000

    Create main.tf

---
+++ envs/nonprod/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "s3" {
+    bucket         = "konpal-tf-state-bucket-12345"  # from backend step
+    key            = "nonprod/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "konpal-tf-locks"
+    encrypt        = true
+  }
+}
+
+provider "aws" { region = "us-east-1" }
+
+resource "aws_s3_bucket" "demo" {
+  bucket = "konpal-nonprod-demo-12345"
+  acl    = "public-read"
+}
