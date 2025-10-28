768ad4664df18d250f7203bfedaa89a62d1b6fc5
KonpalM <138207736+KonpalM@users.noreply.github.com>
2025-10-28 23:31:57 +0000

    Update main.tf

--- envs/nonprod/main.tf
+++ envs/nonprod/main.tf
@@ -10,7 +10,7 @@ terraform {
 
 provider "aws" { region = "us-east-1" }
 
-resource "aws_s3_bucket" "demo" {
+resource "aws_s3_bucket_acl" "demo" {
   bucket = "konpal-nonprod-demo-12345"
   acl    = "public-read"
 }
