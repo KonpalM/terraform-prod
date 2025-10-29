2f3bc813e70c91d08ccb349c5564d111bcf766d6
KonpalM <138207736+KonpalM@users.noreply.github.com>
2025-10-29 00:10:30 +0000

    Update main.tf

--- envs/nonprod/main.tf
+++ envs/nonprod/main.tf
@@ -1,16 +1,21 @@
-terraform {
-  backend "s3" {
-    bucket         = "konpal-tf-state-bucket-12345"  # from backend step
-    key            = "nonprod/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "konpal-tf-locks"
-    encrypt        = true
-  }
+provider "aws" {
+  region = "us-east-1
 }
 
-provider "aws" { region = "us-east-1" }
+resource "aws_s3_bucket" "demo" {
+  bucket = "konpal-public-demo-12345"
+}
 
-resource "aws_s3_bucket_acl" "demo" {
-  bucket = "konpal-nonprod-demo-12345"
-  acl    = "public-read"
+resource "aws_s3_bucket_policy" "public_policy" {
+  bucket = aws_s3_bucket.demo.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = "*"
+      Action    = "s3:GetObject"
+      Resource  = "${aws_s3_bucket.demo.arn}/*"
+    }]
+  })
 }
